38234301aa07e7e0d5fd14559e6222dcc1bb7ba5
unknown <angelcobian@outlook.com>
2018-06-05 16:32:43 -0700

    string-interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { Component, OnInit } from '@angular/core';
+// this is importing the info form the file and it named Person.
+import { Person } from './classes/person';
+ @Component({
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+title = 'app';
+// creating new people. Using the method shown to us on firday?
+hello: Person[] = [
+  new Person('Mrs/Mr','hello','World',''),
+  new Person('Mr/Mrs', 'World', 'Hello', 'Coalinga')
+];
+
+
+// this is a used on {{heading}} in html page
+heading = 'Person Details';
+// Person is the being used for the constructor and piping in the info form file
+Person: any;
+
+// this is setting up newpl with info for the constructor
+// public newpl: Person = new Person(); // Nevermind
+
+
+// this is for display in html
+result: any;
+
+// this setting up to display.
+constructor(){
+  this.result = this.hello;
+  // just for test
+  // console.log(this.newpl.peoples)
+}
+
+ngOnInit() {
+
+}
 }
---
+++ src/app/classes/person.ts
@@ -0,0 +1,20 @@
+
+// Im just a file with some people info
+export class Person {
+  // everyone can see my array
+  // public peoples: any[] = [{
+  //   gender:"Mr./Ms.",
+  //   firstname: 'hello',
+  //   lastname: 'world',
+  //   city: 'dfgvbhnj',
+
+  // },
+  // {
+  //   new peoples(2,4,1)
+  // tslint:disable-next-line:semicolon
+
+  // Nevermind now im just a setup for people(s) info
+  constructor(public gender: any, public firstname: string, public lastname: string, public city: string) {}
+  }
+
+
